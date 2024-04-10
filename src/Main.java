fc01e3be1c4adac55424b6c9d46acee358cf4d4c
Vigneshwar Balaji <vickycloud2810@gmail.com>
2024-04-10 19:11:13 +0530

    feat: commenting out the code for proper understanding

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //Generate Random number and print only first 10 distinct sorted numbers using streams
         GenerateRandomNumbers generateRandomNumbers = new GenerateRandomNumbers();
         int randomNumbers [] = generateRandomNumbers.generateRandomNumbersAndPrintOnlyFirstTenDistinctNumbersUsingStreams();
         for(int number : randomNumbers){
             System.out.println(number);
         }
+
+        // count the number of characters in a given string
         JavaStreamPractice javaStreamPractice = new JavaStreamPractice();
         String count = javaStreamPractice.stringCharacterCounter("NewtonianDynamics");
         System.out.println(count);
 
+        /*
+            Employee POJO - id, name, salary, grade, dept.
+            In a list of employees write a Java 8 program
+            using Stream API to give 10% salary hike to those in HR dept. and Grade > 6.
+        */
+
+        //Creating pojo and assigning objects
         Employee employee1 = new Employee(0, "Rob", 1000, 2, "Tech");
         Employee employee2 = new Employee(1, "Jin", 2000, 7, "HR");
         Employee employee3 = new Employee(2, "Veronica", 1000, 1, "Marketing");
@@ -20,6 +30,7 @@ public class Main {
         Employee employee5 = new Employee(4, "Sam", 4000, 3, "Tech");
         Employee employee6 = new Employee(6, "Mary", 1000, 2, "Management");
 
+        // Give the salary hike to relevant employees and print all the employees
         ArrayList<Employee> listOfEmployees = new ArrayList<>();
         Collections.addAll(listOfEmployees, employee1, employee2, employee3, employee4, employee5, employee6);
         EmployeeSalaryAppUsingStreams employeeSalaryAppUsingStreams = new EmployeeSalaryAppUsingStreams();
